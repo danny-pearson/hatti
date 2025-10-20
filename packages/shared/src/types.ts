341801cbc68f1824bc33ce140b273e1de0f940b8
dannypearson <danny@crtr.co.uk>
2025-10-20 01:47:09 +0100

    Adds `TypedArray` union type to shared package

--- packages/shared/src/types.ts
+++ packages/shared/src/types.ts
@@ -11,3 +11,14 @@ export type Tuple4<T> = [T, T, T, T];
 export type Mutable<Type> = {
     -readonly [Key in keyof Type]: Type[Key];
 };
+
+export type TypedArray = Int8Array
+        | Uint8Array
+        | Uint8ClampedArray
+        | Int16Array
+        | Uint16Array
+        | Int32Array
+        | Uint32Array
+        | Float16Array
+        | Float32Array
+        | Float64Array;
