6465e63c6850fd9f93fa9f4c1a8396ddb600a3fa
dannypearson <danny@crtr.co.uk>
2025-10-23 23:26:09 +0100

    Removes `PermutedCG`. Can't work correctly without 64bit state

--- packages/random/src/index.ts
+++ packages/random/src/index.ts
@@ -1,7 +1,6 @@
 export * from './utils.js';
 export * as prng from './utils.js';
 export * from './types.js';
-export { default as PermutedCG } from './PermutedCG.js';
 export { default as QuadraticCG } from './QuadraticCG.js';
 export { default as XorShift } from './XorShift.js';
 export { default as Mulberry32 } from './Mulberry32.js';
